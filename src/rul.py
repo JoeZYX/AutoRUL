4ad55fb4645fecc2eb5f86e05f8bb25ea72be077
André Gilbert <andre-anan.gilbert@sap.com>
2021-03-16 10:20:24 +0100

    refactored code

--- src/rul.py
+++ src/rul.py
@@ -27,22 +27,18 @@ class RemainingUsefulLife:
 
     Attributes:
         data: Data to predict the remaining useful life for.
-        data_id:
+        data_id: ID of the given dataframe.
         sequence_length:
         window_size:
-        model_id:
+        model_id: Generated uuid.
         train_FD:
         test_FD:
         X_batch:
         y_batch:
         number_of_sensors:
+        log_dir:
     """
-
-    def __init__(self,
-                 data: pd.DataFrame,
-                 data_id: str = '',
-                 sequence_length: int = 5,
-                 window_size: int = 16) -> None:
+    def __init__(self, data: pd.DataFrame, data_id: str = '', sequence_length: int = 5, window_size: int = 16) -> None:
         """Inits RemainingUsefulLife."""
         self.__data = data
         self.__data_id = data_id
@@ -55,6 +51,7 @@ class RemainingUsefulLife:
         self.__X_batch = None
         self.__y_batch = None
         self.__number_of_sensors = None
+        self.__log_dir = None
 
     def rul(self, max_life: int) -> None:
         id = 'engine_id'
@@ -132,56 +129,49 @@ class RemainingUsefulLife:
         self.__number_of_sensors = self.__X_batch.shape[-2]
 
     def initialize_model(self) -> None:
-        valid = check_the_config_valid(architecture_parameters,
-                                       self.__window_size,
-                                       self.__number_of_sensors)
+        valid = check_the_config_valid(architecture_parameters, self.__window_size, self.__number_of_sensors)
         if valid:
-            self.__model = build_the_model(architecture_parameters,
-                                           self.__sequence_length,
-                                           self.__window_size,
+            self.__model = build_the_model(architecture_parameters, self.__sequence_length, self.__window_size,
                                            self.__number_of_sensors)
         else:
             print("invalid configuration")
 
-        input_data = tf.keras.layers.Input(
-            shape=(self.__sequence_length, self.__window_size, self.__number_of_sensors, 1))
+        input_data = tf.keras.layers.Input(shape=(self.__sequence_length, self.__window_size, self.__number_of_sensors,
+                                                  1))
         out = self.__model(input_data)
         print(self.__model.summary())
 
-    def train_model(self, data_id, model, x_batch, y_batch) -> None:
+    def train_model(self) -> None:
         date_time_obj = datetime.now()
-        log_dir = "logs/{}_{}_{}_{}_{}_{}_{}/".format(data_id, date_time_obj.year,
-                                                      date_time_obj.month,
-                                                      date_time_obj.day,
-                                                      date_time_obj.hour,
-                                                      date_time_obj.minute,
-                                                      date_time_obj.second)
+        self.__log_dir = "logs/{}_{}_{}_{}_{}_{}_{}/".format(self.__data_id, date_time_obj.year, date_time_obj.month,
+                                                             date_time_obj.day, date_time_obj.hour,
+                                                             date_time_obj.minute, date_time_obj.second)
 
-        os.makedirs(log_dir)
+        os.makedirs(self.__log_dir)
 
-        checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
+        checkpoint = ModelCheckpoint(self.__log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
                                      monitor='val_loss',
                                      save_weights_only=True,
                                      save_best_only=True)
         reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.3, patience=5, verbose=1)
         early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=15, verbose=1, mode='auto')
-        model.fit(x_batch,
-                  y_batch,
-                  batch_size=15,
-                  epochs=30,
-                  callbacks=[checkpoint, reduce_lr, early_stopping],
-                  validation_split=0.075)
+        self.__model.fit(self.__X_batch,
+                         self.__y_batch,
+                         batch_size=15,
+                         epochs=30,
+                         callbacks=[checkpoint, reduce_lr, early_stopping],
+                         validation_split=0.075)
 
-        model.save_weights(log_dir + 'trained_weights_final.h5')
+        self.__model.save_weights(self.__log_dir + 'trained_weights_final.h5')
 
-    def evaluate_model(self, log_dir, x_batch) -> None:
-        model_list = os.listdir(log_dir)
+    def evaluate_model(self) -> None:
+        model_list = os.listdir(self.__log_dir)
         model_list = [file for file in model_list if "val_loss" in file]
-        self.__model.load_weights(log_dir + model_list[-1])
+        self.__model.load_weights(self.__log_dir + model_list[-1])
 
-        y_batch_pred = self.__model.predict(x_batch)
+        y_batch_pred = self.__model.predict(self.__X_batch)
         y_batch_pred = y_batch_pred.reshape(y_batch_pred.shape[0], y_batch_pred.shape[1])
-        y_batch_reshape = y_batch.reshape(y_batch.shape[0], y_batch.shape[1])
+        y_batch_reshape = self.__y_batch.reshape(self.__y_batch.shape[0], self.__y_batch.shape[1])
         rmse_on_train = np.sqrt(mean_squared_error(y_batch_pred, y_batch_reshape))
 
         print(f'The RMSE on Training dataset {self.__data_id} is {rmse_on_train}.')
@@ -203,4 +193,5 @@ class RemainingUsefulLife:
         print(f'The RMSE on test dataset {self.__data_id} is {rmse_on_test}.')
 
     def get_model(self) -> object:
-        return self.__model.copy(deep=True)
+        """Returns the model."""
+        return self.__model
