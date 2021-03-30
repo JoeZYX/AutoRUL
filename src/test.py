c59747bb742c53724e196f70122b4534d950124a
André Gilbert <andre-anan.gilbert@sap.com>
2021-03-30 18:50:55 +0200

    added results

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ from rul import RemainingUsefulLife
 if __name__ == '__main__':
     #print(os.listdir('src'))
     rul = RemainingUsefulLife("CMAPSSData")
-    rul.auto_rul(["FD001"], ["FD001"], ["FD001"])
+    rul.auto_rul(["FD002"], ["FD002"], ["FD002"])
     rul.test()
     print(rul.get_results())
-    rul.export_to_csv('FD001')
+    rul.export_to_csv('FD002')
