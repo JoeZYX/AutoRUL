e3daaf8377408f6bb9bf722a51f1751d11873ca8
André Gilbert <andre-anan.gilbert@sap.com>
2021-04-01 16:04:59 +0200

    trained fold 4

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ from rul import RemainingUsefulLife
 if __name__ == '__main__':
     #print(os.listdir('src'))
     rul = RemainingUsefulLife("CMAPSSData")
-    rul.auto_rul(["FD002"], ["FD002"], ["FD002"])
+    rul.auto_rul(["FD004"], ["FD004"], ["FD004"])
     rul.test()
     print(rul.get_results())
-    rul.export_to_csv('FD002')
+    rul.export_to_csv('FD004')
