84b4e65e34b67c5f6914bc47364e7e1b230a0ab8
Valentin Müller <valentin.mueller@sap.com>
2021-04-01 15:20:43 +0200

    Added two predictions

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ from rul import RemainingUsefulLife
 if __name__ == '__main__':
     #print(os.listdir('src'))
     rul = RemainingUsefulLife("CMAPSSData")
-    rul.auto_rul(["FD005"], ["FD005"], ["FD005"])
+    rul.auto_rul(["FD002"], ["FD002"], ["FD002"])
     rul.test()
     print(rul.get_results())
-    rul.export_to_csv('FD005')
+    rul.export_to_csv('FD002')
