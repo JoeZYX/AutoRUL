b03ffbfc550322234d315587ecc43964accd82b9
Valentin Müller <valentin.mueller@sap.com>
2021-04-01 17:22:50 +0200

    Predictions for truncated fold 1

--- src/test.py
+++ src/test.py
@@ -4,8 +4,9 @@ from rul import RemainingUsefulLife
 
 if __name__ == '__main__':
     #print(os.listdir('src'))
+    data = "FD001_WT_150"
     rul = RemainingUsefulLife("CMAPSSData")
-    rul.auto_rul(["FD004"], ["FD004"], ["FD004"])
+    rul.auto_rul(["FD001"], [data], [data])
     rul.test()
     print(rul.get_results())
-    rul.export_to_csv('FD004')
+    rul.export_to_csv(data)
